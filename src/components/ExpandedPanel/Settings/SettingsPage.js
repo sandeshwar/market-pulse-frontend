7b42403070cb75fc473da77be0c3cb8e4e4ce62f
Sandeshwar Sharma <dev.sandeshwar@outlook.com>
2025-02-11 21:51:39 +0530

    Remove unused settings and utility modules

--- src/components/ExpandedPanel/Settings/SettingsPage.js
+++ src/components/ExpandedPanel/Settings/SettingsPage.js
@@ -1,8 +1,4 @@
-import { createThresholdSettings } from './ThresholdSettings.js';
-import { createDisplaySettings } from './DisplaySettings.js';
 import { createListManagementSettings } from './ListManagementSettings.js';
-import { createCard } from '../../common/Card/Card.js';
-import { ICONS } from '../../../utils/icons.js';
 
 export function createSettingsPage() {
   return `
